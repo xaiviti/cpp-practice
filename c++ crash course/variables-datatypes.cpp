6fb33b8f9b0b03664818f96de1305db61535f2b8
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 01:04:53 +0800

    variables-dataTypes-commit-2

--- c++ crash course/variables-datatypes.cpp
+++ c++ crash course/variables-datatypes.cpp
@@ -7,4 +7,16 @@ using namespace std;
 
 int main(){
 	
+	// Mathematical problems
+	//data types
+	//integer (int) = whole number
+	//double (double) = decimal
+	//character (char) = 1 character (letter or symbol)
+	//string (string) = 1 or more characters (name)
+	//boolean (bool) = true or false
+	int x = 2022;
+	cout << x+" is an integer\n";
+	
+	return 0;
+	
 }
