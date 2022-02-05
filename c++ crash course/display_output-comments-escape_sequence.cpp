6b941f87813da0b74d088a7a6a0af402a6975e8c
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 00:14:55 +0800

    main-display-text-to-console-commit-5

--- c++ crash course/display_output-comments-escape_sequence.cpp
+++ c++ crash course/display_output-comments-escape_sequence.cpp
@@ -9,9 +9,19 @@ using namespace std; //namespace - can reuse same name
 
 int main(){
 	
-	cout << "Baby, I'm falling, head over heels\n";
-	cout << "Looking for ways to let you know just how I feel\n";
+	cout << "It's you by Henry\n\n";
+	
+	cout << "Baby, I'm falling, head over heels" << endl;
+	cout << "Looking\t for\t ways\t to let you know just how I feel\n";
+	cout << "\"I wish I was holding you by my side\"\n";
+	cout << "\\I wouldn't change a thing 'cause finally it's real\n";
 	
 	return 0; //exit status (success status) - not required in c++
 	// "\n" or (<< endl;) ->line breaks: to enter the next line 
+	// "\t" -> for tab
+	// \" adding double quotes
+	// \\ for back slash
+	//comments: portion of code that is ignored by the compiler
+	
+	
 }
