c54a24f423c5ab3991712f028a7625f537f7ccb0
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 01:16:17 +0800

    variables-dataTypes-commit-6

--- c++ crash course/variables-datatypes.cpp
+++ c++ crash course/variables-datatypes.cpp
@@ -20,6 +20,7 @@ int main(){
 	int year = 2022;
 	cout << "Year: " << year << endl;		//int
 	double priceTag = 99.89;
+	priceTag = priceTag * 0.5;
 	cout << "Amount is " << priceTag << endl;	//double
 	char onlineStatus = 'G';
 	cout << "Color Status is " << onlineStatus << endl;//character
