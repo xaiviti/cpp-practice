b36592b472f2784813af979f1f654f5b47796aed
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 01:09:33 +0800

    variables-dataTypes-commit-3

--- c++ crash course/variables-datatypes.cpp
+++ c++ crash course/variables-datatypes.cpp
@@ -14,8 +14,16 @@ int main(){
 	//character (char) = 1 character (letter or symbol)
 	//string (string) = 1 or more characters (name)
 	//boolean (bool) = true or false
-	int x = 2022;
-	cout << x+" is an integer\n";
+	int year = 2022;
+	cout << year << endl;		//int
+	double priceTag = 99.89;
+	cout << priceTag << endl;	//double
+	char onlineStatus = 'G';
+	cout << onlineStatus << endl;//character
+	string firstName = "Heidi";
+	cout << firstName << endl;	//string
+	bool online = true;
+	cout << online << endl;		//boolean
 	
 	return 0;
 	
