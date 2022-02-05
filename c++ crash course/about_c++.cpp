efc24f4c8b0a7e5daad68d6d2c8a6f52a891c493
xaiviti <heidzie.siapco@gmail.com>
2022-02-05 21:51:15 +0800

    about c++

---
+++ c++ crash course/about_c++.cpp
@@ -0,0 +1,12 @@
+/*
+C++ advantages
+	-	fast! (great for games)
+	-	portable (Windows, Mac, Linux)
+	-	supports object-oriented programming
+	
+C++ disadvantages
+	-	difficult to learn (not recommended 1st language)
+	-	use of pointers (misuse may make system crash)
+	-	no security
+	-	no garbage collection
+*/
