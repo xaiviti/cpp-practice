379168f21b2670cae1d8740c5e1b1150a46b2e23
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 00:16:48 +0800

    main-display-text-to-console-commit-7

--- c++ crash course/display_output-comments-escape_sequence.cpp
+++ c++ crash course/display_output-comments-escape_sequence.cpp
@@ -16,6 +16,60 @@ int main(){
 	cout << "\"I wish I was holding you by my side\"\n";
 	cout << "\\I wouldn't change a thing 'cause finally it's real\n";
 	
+	/*
+	I'm tryin' to hold back, you ought to know that
+	You're the one that's on my mind
+	Falling too fast, deeply in love
+	Finding the magic in the colors of you
+	You're the right time
+	At the right moment
+	You're the sunlight
+	Keeps my heart going, oh-whoa
+	Know when I'm with you
+	I can't keep myself from falling
+	Right time at the right moment
+	It's you
+	You, it's you
+	You, it's you
+	Baby, I'm falling, oh, deeper in love
+	Everything that you are was all I'm dreaming of, yeah
+	And if I can break enough to show you that I need us
+	I'd give up everything I have, girl, just for you, oh-whoa
+	Tryin' to hold back, you ought to know that
+	You're the one that's on my mind
+	Falling too fast, deeply in love
+	Girl, all I need to breathe is you
+	'Cause you're the right time
+	At the right moment
+	You're the sunlight
+	Keeps my heart going, oh-oh-whoa
+	I know when I'm with you
+	I can't keep myself from falling
+	Right time at the right moment
+	It's you
+	Every night in my bed, I'm dreaming (oh-whoa)
+	That it's you in my arms I'm holding (oh-whoa)
+	Girl, all I want is you
+	You know you're the right time
+	At the right moment
+	You're the sunlight
+	Keeps my heart going
+	It's you, you, you, yeah
+	'Cause you're the right time
+	At the right moment (the moment)
+	You're the sunlight (sunlight)
+	Keeps my heart going, oh-oh-whoa (yeah, yeah)
+	I know when I'm with you (with you, baby)
+	I can't keep myself from falling (falling)
+	Right time at the right moment
+	(Girl) it's you
+	You (you), it's you
+	(It's you) you, (no, no) it's you
+	(Whoa-oh-whoa) you (it's you, love), it's you
+	(It's you, yeah, yeah) you
+	You, it's you
+	*/
+	
 	return 0; //exit status (success status) - not required in c++
 	// "\n" or (<< endl;) ->line breaks: to enter the next line 
 	// "\t" -> for tab
