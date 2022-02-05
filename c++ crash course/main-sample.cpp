31c636929d89f713384a379a129c185a0723cea1
xaiviti <heidzie.siapco@gmail.com>
2022-02-05 23:22:04 +0800

    main-display-text-to-console-commit-2

--- c++ crash course/main-sample.cpp
+++ c++ crash course/main-sample.cpp
@@ -4,5 +4,10 @@ Main - Text on console
 
 // header file library
 #include <iostream>	//iostream - for input and output
+using namespace std;
 
-
+//main method - entrance point to the program
+int main(){
+	cout << "It's You By Henry\n\n";
+	
+}
