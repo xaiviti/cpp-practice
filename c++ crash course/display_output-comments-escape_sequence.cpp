53d9a8ff2babe3c23211135cfcaea28896a43f44
xaiviti <heidzie.siapco@gmail.com>
2022-02-05 23:58:37 +0800

    main-display-text-to-console-commit-4

---
+++ c++ crash course/display_output-comments-escape_sequence.cpp
@@ -0,0 +1,17 @@
+/*
+	-displaying output
+	-comments
+	-escape-sequence
+*/
+
+#include <iostream>
+using namespace std; //namespace - can reuse same name
+
+int main(){
+	
+	cout << "Baby, I'm falling, head over heels\n";
+	cout << "Looking for ways to let you know just how I feel\n";
+	
+	return 0; //exit status (success status) - not required in c++
+	// "\n" or (<< endl;) ->line breaks: to enter the next line 
+}
