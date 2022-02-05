98047dd70462a48fb7f39b44343ad5cf4921e529
xaiviti <heidzie.siapco@gmail.com>
2022-02-05 21:57:42 +0800

    about c++ commit-2

--- c++ crash course/about_c++.cpp
+++ c++ crash course/about_c++.cpp
@@ -9,4 +9,16 @@ C++ disadvantages
 	-	use of pointers (misuse may make system crash)
 	-	no security
 	-	no garbage collection
+	
+C++ uses
+	-	used for MANY popular applications (web browsers, databases, desktop apps)
+	- 	many game engines use c++ (Ex. Unreal engine)
+	-	used for many embedded systems (electronic devices, microcontrollers)
+
+What will you need to start c++?
+	-	Pick an IDE(Integrated Development Environment)
+		-	Microsoft Visual Studio
+		-	Eclipse for C/C++ developers
+		-	Code Blocks
+	
 */
