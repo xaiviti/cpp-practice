75494ef02c5184d6f07d80924fb40c4aec4cc52a
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 20:55:17 +0800

    logical-operators-commit-1

---
+++ c++ crash course/logical-operators.cpp
@@ -0,0 +1,16 @@
+/*
+Logical Operators
+	- && AND logical operator
+	- || OR logical operator
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
