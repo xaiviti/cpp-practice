b6dbeb565bde15f5423ead640758b85454c4cd42
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 16:35:27 +0800

    if-else-statement-temperature-evaluation-commit-2

--- c++ crash course/temperature-evaluation.cpp
+++ c++ crash course/temperature-evaluation.cpp
@@ -5,6 +5,21 @@ if-else statement: temperature evaluation
 using namespace std;
 
 int main(){
+	//declaration
+	int temperature;
+	
+	//input
+	cout << "TEMPERATURE EVALUATION\n\n";
+	cout << "Please enter the temperature: ";
+	cin >> temperature;
+	
+	if(temperature>=30){
+		cout << "It is HOT outside, :/";
+	}else if(temperature<=0){
+		cout << "It is COLD outside, *brrrrr*";
+	}else{
+		cout << "It is warm outside.";
+	}
 	
 	return 0;
 }
