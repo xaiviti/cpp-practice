1c5f302220251f03de89b85fadc401c5cefb5e03
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 21:32:18 +0800

    logical-operators-commit-2

--- c++ crash course/logical-operators.cpp
+++ c++ crash course/logical-operators.cpp
@@ -11,6 +11,32 @@ Logical Operators
 using namespace std;
 
 int main(){
+	/*
+	double temp;
 	
+	cout << "Enter your temperature: ";
+	cin >> temp;
+	
+	// && 
+	if(temp>=35 && temp<=37.5){
+		cout << "You're temperature is normal. ";
+	}else {
+		cout << "GO TO THE HOSPITAL!";
+	}
+	*/
+	
+	string cough;
+	
+	cout << "Do you have a cough (yes/no): ";
+	cin >> cough;
+	
+	// ||
+	if(cough.at(0)=='y' || cough.at(0)=='Y'){
+		cout << "You are sick!";
+	}else{
+		cout << "You are probably fine.";
+	}
+	
+	cout << endl;
 	return 0;
 }
