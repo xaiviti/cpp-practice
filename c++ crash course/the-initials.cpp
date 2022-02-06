63f64dd254a4796cdfc4910dc0ab9a54d9de61c7
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 18:36:45 +0800

    the-initials-commit-1

--- c++ crash course/the-initials.cpp
+++ c++ crash course/the-initials.cpp
@@ -1,11 +1,28 @@
 /*
-Intials from the first name, middle name and last name
+Extract intials from the first name, middle name and last name
 */
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main(){
+	//declaration
+	string firstName, middleName, lastName;
+	char first=firstName.at(0);
+	char middlemiddleName.at(0);
+	char last=lastName.at(0);	
+	
+	//input
+	cout << "EXTRACTING THE INITIALS\n";
+	cout << "Enter your first name: ";
+	cin >> firstName;
+	cout << "Enter your middle name: ";
+	cin >> middleName;
+	cout << "Enter your last name: ";
+	cin >> lastName;
+	
+	//output
+	
 	
 	return 0;
 }
