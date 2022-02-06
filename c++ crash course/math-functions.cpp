20336224fdb7ed0d59a78592988589c57c78c1a1
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 20:14:01 +0800

    math-functions-commit-2

--- c++ crash course/math-functions.cpp
+++ c++ crash course/math-functions.cpp
@@ -3,10 +3,28 @@ Math Functions
 */
 
 #include <iostream>
-#include <math>
+#include <string>
+#include <algorithm>		//import algorithm
 using namespace std;
 
 int main(){
+	//declaration	
+	double x, y;
+	
+	//input
+	cout << "MATH FUNCTIONS\n\n";
+	cout << "Enter number 1: ";
+	cin >> x;
+	cout << "Enter number 2: ";
+	cin >> y;
+	
+	//min and max functions
+	double z = max(x,y);
+	double w = min(x,y);
+	
+	//return the higher number
+	cout << "Higher Number is " << z;
+	cout << "Lower Number is " << w;
 	
 	return 0;
 }
