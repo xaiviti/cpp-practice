6ece58eeaea3c4cb5dee4c485b66cdf9a4505086
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 18:32:37 +0800

    string-functions-commit-2

--- c++ crash course/string-functions.cpp
+++ c++ crash course/string-functions.cpp
@@ -7,17 +7,33 @@ using namespace std;
 
 int main(){
 	//declaration
-	string firstName;
+	string firstName, lastName, middleName, greetings;
 	
 	//input
 	cout << "STRING FUNCTIONS\n\n";
 	
+	cout << "Enter Welcome: ";
+	getline(cin, greetings);
 	cout << "Enter your first name: ";
 	getline(cin, firstName);
+	cout << "Enter your middle name: ";
+	getline(cin, middleName);
+	cout << "Enter your last name (do not enter anything): ";
+	getline(cin, lastName);
 	
 	// stringName.length() returns the length of the string
 	cout << "Length of first name is " << firstName.length();
+	cout << endl;
+	// stringName.empty() checks if the first name is empty
+	cout << "Is the Last Name empty? " << lastName.empty();
+	cout << endl;
+	// stringName.clear() clears the value of the string
+	middleName.clear();
+	cout << "Middle Name is " << middleName;
 	
+	cout << endl;
+	// stringName.append(stringName2) appends two strings
+	cout << firstName.append("@gmail.com");
 	
 	return 0;
 }
---
+++ c++ crash course/the-initials.cpp
@@ -0,0 +1,11 @@
+/*
+Intials from the first name, middle name and last name
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
