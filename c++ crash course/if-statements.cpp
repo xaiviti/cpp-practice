111106fa389cbdd87f60a3af835cc9cd90839a5f
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 16:05:00 +0800

    if-statements-commit-1

---
+++ c++ crash course/if-statements.cpp
@@ -0,0 +1,13 @@
+/*
+if statements - basic form of decision making
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	int age = 18;
+	if(age==18){
+		cout << "Congrats! You're 18 years old.";
+	}
+	return 0;
+}
