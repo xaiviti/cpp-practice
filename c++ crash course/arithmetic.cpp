6b8b622b510ab34831cc33da79cd19e165bfdfba
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 15:17:32 +0800

    arithmetic-commit-2

--- c++ crash course/arithmetic.cpp
+++ c++ crash course/arithmetic.cpp
@@ -6,6 +6,19 @@ Arithemetic
 using namespace std;
 
 int main(){
+	//Arithmetics
+	//addition			+
+		//ex. cout << 5 + 2;
+	//subtraction		-
+		// ex. cout << 5 - 2;
+	//multiplication	*
+		//ex .cout << 5*2;
+	//division			/
+		// ex. cout << 5/2; //displays only the whole no.(2)
+	//modulus			%:it gets the remainder
+		// ex. cout << 5 % 2; // remainder of 5/2 = 1
+	
+	
 	
 	return 0;
 }
