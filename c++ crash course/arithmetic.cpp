81431b7ed83ffce02b70b515ed85ff3cb55f6984
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 15:48:50 +0800

    arithmetic-commit-3

--- c++ crash course/arithmetic.cpp
+++ c++ crash course/arithmetic.cpp
@@ -1,23 +1,37 @@
 /*
 Arithemetic
-	-
 */
 #include <iostream>
 using namespace std;
 
 int main(){
-	//Arithmetics
-	//addition			+
-		//ex. cout << 5 + 2;
-	//subtraction		-
-		// ex. cout << 5 - 2;
-	//multiplication	*
-		//ex .cout << 5*2;
-	//division			/
-		// ex. cout << 5/2; //displays only the whole no.(2)
-	//modulus			%:it gets the remainder
-		// ex. cout << 5 % 2; // remainder of 5/2 = 1
+	//Arithmetic Operators
+		//addition			+
+			//ex. cout << 5 + 2; // results is 7
+		//subtraction		-
+			// ex. cout << 5 - 2; // results is 3
+		//multiplication	*
+			//ex .cout << 5*2; // results is 10
+		//division			/
+			// ex. cout << 5/2; //displays only the whole no.(2)
+		//modulus			%:it gets the remainder
+			// ex. cout << 5 % 2; // remainder of 5/2 = 1
 	
+	//Increment and Decrement Operators
+		/*
+		//Increment
+			int total = 1;
+			total+=2;
+			cout << total;
+		//Decrement
+			int total = 1;
+			total--;
+			cout << total;
+		*/
+	
+	//Programming Equations P(M|d)(A|S): left to right eval
+	double total = 1 + 2 -3 * 4 / 5.0 + (6 + 7);
+	cout << total;
 	
 	
 	return 0;
