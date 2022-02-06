e854d7d9d95850ccd73334921ee5e3d498dd6600
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 14:23:56 +0800

    accepting-user-input-commit2

--- c++ crash course/accepting-user-input.cpp
+++ c++ crash course/accepting-user-input.cpp
@@ -6,5 +6,14 @@ using namespace std;
 
 int main(){
 	
+	//accept user input
+	//store it on a variable and use it
+	string name;
 	
+	cout << "Accepting user input\n\n";
+	cout << "Enter you name: ";
+	cin >> name;
+	cout << "Welcome " << name + "!";
+	
+	return 0;
 }
