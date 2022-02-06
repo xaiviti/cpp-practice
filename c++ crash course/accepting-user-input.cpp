7dc689ddc843f9f33f45eaecef014629acf44f83
xaiviti <heidzie.siapco@gmail.com>
2022-02-06 14:40:46 +0800

    accepting-user-input-commit3

--- c++ crash course/accepting-user-input.cpp
+++ c++ crash course/accepting-user-input.cpp
@@ -2,6 +2,7 @@
 	-accepting user input
 */
 #include <iostream>
+#include <string> //for getline function: in order to get multiple words
 using namespace std;
 
 int main(){
@@ -9,11 +10,15 @@ int main(){
 	//accept user input
 	//store it on a variable and use it
 	string name;
+	int age;
 	
 	cout << "Accepting user input\n\n";
 	cout << "Enter you name: ";
-	cin >> name;
+	getline(cin, name);// replace cin statement with getline
+	cout << "Enter your age: ";
+	cin >> age;
 	cout << "Welcome " << name + "!";
+	cout << " Your age is " << age;
 	
 	return 0;
 }
