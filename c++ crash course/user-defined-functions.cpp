f073ee243c2d8f03d990d41b26804469dc68e77d
xaiviti <heidzie.siapco@gmail.com>
2022-02-07 15:25:37 +0800

    user-defined-functions-commmit-2

--- c++ crash course/user-defined-functions.cpp
+++ c++ crash course/user-defined-functions.cpp
@@ -7,9 +7,21 @@ User defined functions
 #include <cmath>
 using namespace std;
 
+//sending information
+void greetings(string name){
+	cout << "Welcome to my program " << name + "!";
+	cout << endl;
+};
+// calling of function
+void goodbye(){
+	cout << "Goodbye!";
+	cout << endl;
+};
+
+
 int main(){
-	
-	
-	
+	greetings("Heidi");
+	goodbye();
+	cout << endl;
 	return 0;
 }
