a321a16d348bc2254ae47e46f80966f38d5649ce
xaiviti <heidzie.siapco@gmail.com>
2022-02-09 00:07:46 +0800

    cpp3-marked-up-selling-price-commit-2

--- solved problems in c++/cpp3-marked-up-selling-price.cpp
+++ solved problems in c++/cpp3-marked-up-selling-price.cpp
@@ -9,12 +9,13 @@ need to find the selling price.
 using namespace std;
 
 class Items {
-	string name;
-	double initialPrice, sellingPrice;
-	
-	void info(){
-		cout << this->name << "\t\t" << this->initialPrice << "\t\t" << this->sellingPrice << endl;
-	}
+	public:
+		string name;
+		double initialPrice, sellingPrice;
+		
+		void info(){
+			cout << this->name << "\t\t" << "$ "<< this->initialPrice << "\t\t" << "$ "<< this->sellingPrice << endl;
+		}
 	
 };
 
@@ -27,7 +28,38 @@ int main(){
 	Items item5;
 	Items item6;
 	
+	item1.name = "Table";
+	item1.initialPrice = 6599;
+	item1.sellingPrice = item1.initialPrice*1.6;
 	
+	item2.name = "Sofa";
+	item2.initialPrice = 2779;
+	item2.sellingPrice = item2.initialPrice*1.6;
+	
+	item3.name = "Bed";
+	item3.initialPrice = 5789;
+	item3.sellingPrice = item3.initialPrice*1.6;
+	
+	item4.name = "Chair";
+	item4.initialPrice = 1299;
+	item4.sellingPrice = item4.initialPrice*1.6;
+	
+	item5.name = "Cabinet";
+	item5.initialPrice = 3800;
+	item5.sellingPrice = item5.initialPrice*1.6;
+	
+	item6.name = "Violin";
+	item6.initialPrice = 4999;
+	item6.sellingPrice = item6.initialPrice*1.6;
+	
+	cout << "Furniture \t Price\t\tSelling Price\n";
+	cout << "---------------------------------------------\n";
+	item1.info();
+	item2.info();
+	item3.info();
+	item4.info();
+	item5.info();
+	item6.info();
 	
 	cout << endl;
 	return 0;
