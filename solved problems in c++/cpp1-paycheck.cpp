e3186f608c845ea20f0abeea04ac914db86dfbe3
xaiviti <heidzie.siapco@gmail.com>
2022-02-08 21:46:36 +0800

    cpp1-paycheck-commit

---
+++ solved problems in c++/cpp1-paycheck.cpp
@@ -0,0 +1,16 @@
+/*
+Problem: Create a program that calculates the monthly paycheck of 
+a salesperson in a department store.
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	
+	
+	
+	cout << endl;
+	return 0;
+}
+
