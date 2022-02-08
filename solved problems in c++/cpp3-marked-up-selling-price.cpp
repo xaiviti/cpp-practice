85787153db1795bb8549d8ffc2c41fa050cd4243
xaiviti <heidzie.siapco@gmail.com>
2022-02-08 23:34:31 +0800

    cpp3-marked-up-selling-price

---
+++ solved problems in c++/cpp3-marked-up-selling-price.cpp
@@ -0,0 +1,16 @@
+/*
+Problem: To make a profit, the prices of the items sold in a furniture 
+store are marked up by 60%. Design an algorithm to find the sellling 
+price of an item sold at the furniture store. What information do you 
+need to find the selling price.
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	
+	
+	cout << endl;
+	return 0;
+}
