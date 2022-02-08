ae6a322185cbe22ab6238cbf2a7e74a15b2fa36f
xaiviti <heidzie.siapco@gmail.com>
2022-02-09 00:48:16 +0800

    cpp4-feet-to-inches-and-centimeters-commit-2

--- solved problems in c++/cpp4-feet-to-inches-and-centimeters.cpp
+++ solved problems in c++/cpp4-feet-to-inches-and-centimeters.cpp
@@ -7,7 +7,19 @@ in inches and cm.
 using namespace std;
 
 int main(){
+	double feet, inches, centimeters;
 	
+	cout << "FEET TO (INCHES AND CENTIMETERS)\n";
+	
+	cout << "Enter value in feet: ";
+	cin >> feet;
+	
+	inches = feet*12;
+	centimeters = feet*30.48;
+	
+	cout << "Result is " << feet << "ft";
+	cout << " is equivalent to " << inches << "in";
+	cout << " and " << centimeters << "cm.";
 	
 	cout << endl;
 	return 0;
