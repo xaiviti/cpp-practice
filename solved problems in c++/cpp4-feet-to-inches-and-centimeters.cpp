6ed86cdbe334682fa6fd34618b7009183a875016
xaiviti <heidzie.siapco@gmail.com>
2022-02-09 00:39:51 +0800

    cpp4-feet-to-inches-and-centimeters

---
+++ solved problems in c++/cpp4-feet-to-inches-and-centimeters.cpp
@@ -0,0 +1,14 @@
+/*
+Problem: Create a program that will convert feet 
+in inches and cm.
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	
+	
+	cout << endl;
+	return 0;
+}
