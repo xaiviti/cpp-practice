b1f8fb026973b05bd3ef1d6e066f1ad7c8d5080e
xaiviti <heidzie.siapco@gmail.com>
2022-02-09 22:24:43 +0800

    cpp6-fibonacci-sequence-commit-2

--- solved problems in c++/cpp6-fibonacci-sequence.cpp
+++ solved problems in c++/cpp6-fibonacci-sequence.cpp
@@ -7,6 +7,25 @@ using namespace std;
 
 int main(){
 	
+	// initiators
+    int num, sum;
+    int present_i = 0;
+    int new_i = 1;
+    //inputs
+    cout << "The Fibonacci Sequence \n";
+    cout << "Enter a number: ";
+    cin >> num;
+    //solution
+    cout << new_i << " ";
+    
+    for(int i=1; i<=(num-1); i++){
+        
+        sum = present_i + new_i; //<-(1+0) - ()
+        cout << sum << " "; 
+        present_i = new_i;
+        new_i = sum;
+        
+    }
 	
 	cout << endl;
 	return 0;
