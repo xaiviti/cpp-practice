f94d554258934992414c3c9faffc1c544e906125
xaiviti <heidzie.siapco@gmail.com>
2022-02-09 23:35:24 +0800

    cpp10-a-or-an

---
+++ solved problems in c++/cpp10-a-or-an.cpp
@@ -0,0 +1,22 @@
+/*
+Problem: Create a program that will output if the word 
+will use a or an, and display the word with the a or an.
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	//declaration
+	string word;
+	
+	//input and process
+	cout << "TELL ME IF I AM A OR AN \n";
+	cout << "Please enter a word: ";
+	cin >> word;
+	
+	
+	
+	cout << endl;
+	return 0;
+}
