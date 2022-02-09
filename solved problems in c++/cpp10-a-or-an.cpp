d8e321e2cff012be89eb469ed72ad06c8d8fa91a
xaiviti <heidzie.siapco@gmail.com>
2022-02-09 23:53:03 +0800

    cpp10-a-or-an-commit-2

--- solved problems in c++/cpp10-a-or-an.cpp
+++ solved problems in c++/cpp10-a-or-an.cpp
@@ -3,7 +3,7 @@ Problem: Create a program that will output if the word
 will use a or an, and display the word with the a or an.
 */
 #include <iostream>
-#include <algorithm>
+#include <string>
 using namespace std;
 
 int main(){
@@ -14,6 +14,19 @@ int main(){
 	cout << "TELL ME IF I AM A OR AN \n";
 	cout << "Please enter a word: ";
 	cin >> word;
+	
+	if(isdigit(word.at(0)) == false){
+		switch(word.at(0)){
+		case 'a': case 'e': case 'i': case 'o': case 'u': case 'A': case 'E': case 'I': case 'O': case 'U': 
+			cout << "An " << word;
+			break;
+		default:
+			cout << "A " << word;
+			break;
+		}
+	}else{
+		cout << "That is not a word.";
+	}
 	
 	
 	
