5248a48cc44f3fd2d253d30d096abe5945949202
xaiviti <heidzie.siapco@gmail.com>
2022-02-09 22:50:17 +0800

    cpp8-factorial-value

---
+++ solved problems in c++/cpp8-factorial-calculator.cpp
@@ -0,0 +1,19 @@
+/*
+Problem: Create a calculator that would process the value of factorial on an input
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	//declaration
+	int number;
+	
+	//input and process
+	cout << "WHAT IS MY FACTORIAL VALUE? \n";
+	cout << "Enter a number: ";
+	cin >> number;
+	
+	cout << endl;
+	return 0;
+}
