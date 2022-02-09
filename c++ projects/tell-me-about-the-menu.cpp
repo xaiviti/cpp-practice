72a6980baa34aa786e8a4398065f64c13731c3a8
xaiviti <heidzie.siapco@gmail.com>
2022-02-10 01:05:10 +0800

    tell-me-about-the-menu

---
+++ c++ projects/tell-me-about-the-menu.cpp
@@ -0,0 +1,22 @@
+/*
+Problem: Create a program that would display the menu of a selected dish.
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	//declaration
+	int choice;
+	
+	//input and process
+	cout << "TELL ME ABOUT THE DISH\n";
+	cout << "Menu: \n";
+	cout << "(1) Fruit Salad \n";
+	cout << "(2) Pesto Pasta \n";
+	cout << "(3) Burger \n";
+	cout << "(4) Pizza \n";
+	
+	cout << endl;
+	return 0;
+}
