39723fb360a5894d30ecbc21ee395a98da2d5323
xaiviti <heidzie.siapco@gmail.com>
2022-02-09 22:57:10 +0800

    cpp8-factorial-value-commit-2

--- solved problems in c++/cpp8-factorial-calculator.cpp
+++ solved problems in c++/cpp8-factorial-calculator.cpp
@@ -8,11 +8,18 @@ using namespace std;
 int main(){
 	//declaration
 	int number;
+	int factorial=1;
 	
 	//input and process
 	cout << "WHAT IS MY FACTORIAL VALUE? \n";
 	cout << "Enter a number: ";
 	cin >> number;
+	
+	for(int i=1; i<=number; i++){
+		factorial = factorial * i;
+	}
+	
+	cout << "Factorial Value: " << factorial;
 	
 	cout << endl;
 	return 0;
