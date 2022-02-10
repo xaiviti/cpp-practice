7dbcd0c6d4d877be8ed6ff10ca1f78bedde8e2e7
xaiviti <heidzie.siapco@gmail.com>
2022-02-11 00:14:08 +0800

    decimal-to-binary-final

--- c++ projects/decimal-to-binary.cpp
+++ c++ projects/decimal-to-binary.cpp
@@ -7,19 +7,34 @@ using namespace std;
 int main(){
 	//declaration
 	int decimal, modulo, quotient;
+	int i=0;
+	int arr[i];
 	
 	//input and process
 	cout << "DECIMAL TO BINARY\n";
 	cout << "Enter the decimal: ";
 	cin >> decimal;
 	
-	do{
+	if(decimal==1){
+		cout << "Binary Value is: " << decimal;
+	}else if (decimal>1){
+		do{
 		modulo = decimal % 2;
-		cout << modulo << " ";
+		arr[i] = modulo;
 		quotient = decimal / 2;
 		decimal = quotient;
-	} while(decimal!=1);
-	cout << quotient;
+		i++;
+		} while(decimal!=1);
+		arr[i] = quotient;
+		cout << "Binary Value is: ";
+		while(i>=0){
+			cout << arr[i] << " ";
+			i--;
+		}
+	}else{
+		cout << "Error!";
+	}
+	
 	
 	cout << endl;
 	return 0;
