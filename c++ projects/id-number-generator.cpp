5d6bca3d878ea6a58a57d932b4af224b97936ffa
xaiviti <heidzie.siapco@gmail.com>
2022-02-11 03:12:33 +0800

    id-generator-commit-2

--- c++ projects/id-number-generator.cpp
+++ c++ projects/id-number-generator.cpp
@@ -3,16 +3,31 @@ Problem: ID NUMBER GENERATOR
 */
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <cstdlib>
 using namespace std;
 
 int main(){
 	//declaration
-	string year_entered, semester_entered, applicant_no;
+	string year_entered , applicant_no;
 	string student_id;
 	
 	//input and process
+	cout << "ID GENERATOR\n";
+	cout << "Enter student year entered the school: ";
+	cin >> year_entered;
+	cout << "Enter student applicant number(XXX): ";
+	cin >> applicant_no;
 	
+	if((!year_entered.empty())&&(year_entered.length()==4)){
+		if((!applicant_no.empty())&&(applicant_no.length()==3)){
+			student_id = year_entered + applicant_no;
+			cout << "Student ID: " << student_id;
+		}else{
+			cout << "INVALID APPLICANT NUMBER!";
+		}
+	}else{
+		cout << "INVALID YEAR!";
+	}
 	
 	
 	cout << endl;
