3a885454a4b4156de64e0ebc86a0206587fbdc09
xaiviti <heidzie.siapco@gmail.com>
2022-02-11 01:16:45 +0800

    source-identifier-with-wavelength

---
+++ c++ projects/source-identifier-from-wavelength.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: Source identifier through wavelength
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	//declaration
+	int wave_length;
+	
+	//input and process
+	cout << "SOURCE IDENTIFIER\n";
+	cout << "Enter the wave length (nanometers): ";
+	cin >> wave_length;
+	
+	
+	
+	cout << endl;
+	return 0;
+}
