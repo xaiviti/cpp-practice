27b9fda2c30c564fb99e0a613122f4347144cb08
xaiviti <heidzie.siapco@gmail.com>
2022-02-11 02:39:06 +0800

    id-generator

---
+++ c++ projects/id-number-generator.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: ID NUMBER GENERATOR
+*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	//declaration
+	string year_entered, semester_entered, applicant_no;
+	string student_id;
+	
+	//input and process
+	
+	
+	
+	cout << endl;
+	return 0;
+}
