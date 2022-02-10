c3fe41dc2f90acf4637f585002806cbf1f589ca5
xaiviti <heidzie.siapco@gmail.com>
2022-02-11 01:42:29 +0800

    nato-code

---
+++ c++ projects/nato-code.cpp
@@ -0,0 +1,18 @@
+/*
+Problem: Convert into NATO CODE
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+	//declaration
+	string message;
+	
+	//input and process
+	cout << "NATO CODE\n";
+	cout << "Enter the message: ";
+	cin >> message;
+	
+	cout << endl;
+	return 0;
+}
